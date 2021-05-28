aba0a3a0048a23b29167a8b06e1afe8535a361aa
Ana Grigalashvili <agrig18@freeuni.edu.ge>
2021-05-28 12:03:16 +0400

    remove unnecessary lines

--- app/src/main/java/ge/agrigalashvili/weatherapp/DataModel.kt
+++ app/src/main/java/ge/agrigalashvili/weatherapp/DataModel.kt
@@ -1,7 +1,5 @@
 package ge.agrigalashvili.weatherapp
 
-import org.json.JSONObject
-import java.util.*
 
 data class WeatherModel (val description: String, val icon: String)
 
@@ -12,8 +10,6 @@ data class MainModel(val temp: Double, val feels_like: Double, val humidity: Int
 data class ListMainModel(val main: MainModel)
 
 data class DateModel (val dt: Long)
-
-//data class ListHourlyModel(val list: List<Object>)
 
 data class HourlyModel (val list: List<ListModel>)
 
